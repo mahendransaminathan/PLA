c24ec95e4209e98df116cdde43bdd2b6a801cd2c
Mahe Sami <mahendransamynathan@gmail.com>
2025-03-27 16:58:25 +0000

    Commit 6: Click on Home button, It should display Welcome to PLA Application Click on Person button, it should display Welcome to Person page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'PLA';
+
+  constructor(private router: Router)
+  {}
+
+  navigateTo(route: string)
+  {
+    this.router.navigate([route]);
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { HomeComponent } from './app/modules/home/home.component';
+import { PersonComponent } from './app/modules/person/person.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, 
+  {
+    providers: [
+      provideRouter([
+        { path: 'home', component: HomeComponent},
+        { path: 'person', component: PersonComponent },
+        { path: '', redirectTo: 'home', pathMatch: 'full'}
+
+      ]
+      )
+    ]
+    })
   .catch((err) => console.error(err));
